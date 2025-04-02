ee4b64e6d909bce612cf89efd651aabcc42f8c3a
aliinescholl <aline.scholl13@gmail.com>
2025-04-01 21:00:23 -0300

    criado vue e navbar inicial

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="width: 100%; display: flex; justify-content: space-between; align-items: center; height: 100%; padding: 30px;">        
+        <div>
+            <ul>
+                
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    ul {
+        display: flex;
+        align-content: end;  
+        list-style: none;
+        gap: 20px;
+    }
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div style="width: 100%; display: flex; justify-content: space-between; align-items: center; height: 100%; padding: 30px;">
+        <div>
+            <h1 style="font-weight: 500;">Aline Scholl</h1>
+        </div>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Skills</li>
+                <li>Projetos</li>
+                <li>Contatos</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    ul {
+        display: flex;
+        align-content: end;  
+        list-style: none;
+        gap: 20px;
+    }
+</style>
